afdac8471f19d7f2c29c8eb32c95d9afe958b79a
Dinesh <dineshaitham2@gmail.com>
2023-02-28 14:22:05 +0530

    added

--- frontend/components/RecieverItem/RecieverItem.jsx
+++ frontend/components/RecieverItem/RecieverItem.jsx
@@ -2,22 +2,31 @@ import Image from "next/image";
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
-const RecieverItem = ({ address, companyName }) => {
+const RecieverItem = ({ address, companyName, name }) => {
   const router = useRouter();
-  
+
   const recieverHandler = () => {
-      router.push(`/stream?userAddress=${address}`)
+    router.push(`/stream?userAddress=${address}`);
   };
   return (
-    <div className="flex items-start justify-between w-[750px] p-6 rounded-2xl shadow-md bg-[#D0E1E9]">
+    <div className="flex items-start justify-between w-[750px] p-6 rounded-2xl shadow-md bg-[#D0E1E9] tracking-wider">
       <div className="flex flex-col gap-5">
-        <p className="font-semibold text-gray-500 font-Poppins flex items-center gap-2">
-          <span>
-            <Image src="/eth.png" height={20} width={20} alt="icon" />
-          </span>
-          {address}
+        <div className="flex gap-5">
+          <p className="text font-Pragati font-bold text-xl">{name}</p>
+
+          <p className="font-semibold text-gray-500 font-Poppins flex items-center gap-2">
+            <span>
+              <Image src="/eth.png" height={20} width={20} alt="icon" />
+            </span>
+            {address}
+          </p>
+        </div>
+        <p className="text font-Pragati font-bold text-xl flex gap-4 ">
+          Company :{" "}
+          <p className="font-semibold text-gray-500 font-Poppins flex items-center gap-2">
+            {companyName}
+          </p>
         </p>
-        <p className="text font-Pragati font-bold text-xl">{companyName}</p>
       </div>
 
       <button
